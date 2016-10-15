85799f9a058020cec83b9557c456dfde2cec380e
dima.mklk <kissmyfullmetalass@gmail.com>
2016-10-15 18:44:58 +0300

    Disabling server logging

--- server.js
+++ server.js
@@ -9,14 +9,13 @@ var port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 app.post('/subscribe', function (req, res) {
   var data = '\r\n' + new Date() + ' ' + req.body.email;
 
   fs.appendFile('subscribers.txt', data, (err) => {
     if (err) throw err;
-    console.log(req.body.email + ' was appended to file!');
   });
 
   res.send('New subscriber: ' + req.body.email);
