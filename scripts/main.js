96fc4a5b735d59b838952f6b24aa4d22846fe606
dima.mklk <kissmyfullmetalass@gmail.com>
2016-10-27 01:47:12 +0300

    s-desktop 1100px and doubletap collection

--- scripts/main.js
+++ scripts/main.js
@@ -30,8 +30,40 @@ $( ".container-footer-subscribe-form-button" ).mousemove(function() {
   }
 });
 
+// Doubletap
+(function($){
+
+  $.event.special.doubletap = {
+    bindType: 'touchend',
+    delegateType: 'touchend',
+
+    handle: function(event) {
+      var handleObj   = event.handleObj,
+          targetData  = jQuery.data(event.target),
+          now         = new Date().getTime(),
+          delta       = targetData.lastTouch ? now - targetData.lastTouch : 0,
+          delay       = delay == null ? 300 : delay;
+
+      if (delta < delay && delta > 30) {
+        targetData.lastTouch = null;
+        event.type = handleObj.origType;
+        ['clientX', 'clientY', 'pageX', 'pageY'].forEach(function(property) {
+          event[property] = event.originalEvent.changedTouches[0][property];
+        })
+
+        // let jQuery handle the triggering of "doubletap" event handlers
+        handleObj.handler.apply(this, arguments);
+      } else {
+        targetData.lastTouch = now;
+      }
+    }
+  };
+
+})(jQuery);
+
 // Collection image viewer
-$( '.container-collection-images-image' ).on('click', function () {
+var viewerEvent = window.innerWidth >= 1100 ? 'click' : 'doubletap';
+$( '.container-collection-images-image' ).on(viewerEvent, function () {
   $('.d-viewer').css('display', 'initial');
 
   var img, imgPrev, imgNext;
@@ -88,7 +120,7 @@ if (isSafari || navigator.platform === 'MacIntel') {
 }
 
 // Special for desktop
-if (window.innerWidth >= 1280) {
+if (window.innerWidth >= 1100) {
   var isAudio = false;
   var isAudioOn = false;
   $( '.container-audio-icon' ).on('click', function () {
